faf3cbfec8bc1dc07f76376d4f87ff69691a1af0
varshith-30 <150704594+vasrhith@users.noreply.github.com>
2024-06-03 06:24:51 +0530

    Add files via upload

---
+++ fib.js
@@ -0,0 +1,14 @@
+function* fibonacciGenerator() {
+    let [prev, curr] = [0, 1];
+    while (true) {
+        yield prev;
+        [prev, curr] = [curr, prev + curr];
+    }
+}
+function generateFibonacci(n) {
+    const fibGen = fibonacciGenerator();
+    for (let i = 0; i < n; i++) {
+        console.log(fibGen.next().value);
+    }
+}
+generateFibonacci(10);
